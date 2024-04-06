2ac9fc687f76808f040e020ba9f8012e0e06c543
webx-downloader <166244237+webx-downloader@users.noreply.github.com>
2024-04-06 19:33:46 +0200

    Add files via upload

---
+++ ddos.js
@@ -0,0 +1,33 @@
+//weboldal háttérszín//
+document.body.style.backgroundColor = "black";
+//weboldal betűszín//
+document.body.style.color = "#00c500"
+
+ console.log("its working")
+        var  btn = document.createElement("button");
+btn.innerHTML = "startup"
+    btn.onclick=function(){
+
+alert("passwordRequired");
+
+    }
+    console.log(location.href);
+console.log("password")
+    
+
+
+
+
+
+
+
+document.body.appendChild(btn);
+
+
+
+
+
+
+
+
+
