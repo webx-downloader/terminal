6d9f77a9ecaea7b7be3f188439e2934677857eb3
webx-downloader <166244237+webx-downloader@users.noreply.github.com>
2024-04-19 17:25:10 +0200

    Add files via upload

--- ddos.js
+++ ddos.js
@@ -4,24 +4,7 @@ document.body.style.backgroundColor = "black";
 document.body.style.color = "#00c500"
 
  console.log("its working")
-        var  btn = document.createElement("button");
-btn.innerHTML = "startup"
-    btn.onclick=function(){
 
-alert("passwordRequired");
-
-    }
-    console.log(location.href);
-console.log("password")
-    
-
-
-
-
-
-
-
-document.body.appendChild(btn);
 
 
 
---
+++ ddos1.js
@@ -0,0 +1,13 @@
+console.log(location.href);
+console.log("password:downloadwebxnow")
+function bestFunction() {
+    let password;
+    if (confirm("passwordRequired") == true) {
+      var x =document.createElement("INPUT");
+      x.setAttribute("type" , "text");
+      document.body.appendChild(x)
+    } else {
+       alert("PASSWORD REQUIRED")
+    }
+   
+  }
