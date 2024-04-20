44c9b7fbb40f22364a763da17df9ea81a8e53a21
webx-downloader <166244237+webx-downloader@users.noreply.github.com>
2024-04-20 21:01:15 +0200

    Add files via upload

--- ddos1.js
+++ ddos1.js
@@ -1,13 +1,22 @@
+//WEB URL!!!!!!!//
 console.log(location.href);
+//DEV : PASSWORD//
 console.log("password:downloadwebxnow")
+//GOMB A HTMLBE IS ÉS ÉRTESÍTÉS COMMAND//
 function bestFunction() {
     let password;
     if (confirm("passwordRequired") == true) {
       var x =document.createElement("INPUT");
+      x.style.position = 'absolute';
+      x.style.left = '100px';
+      x.style.top = '100px';
       x.setAttribute("type" , "text");
       document.body.appendChild(x)
+      const button = document.getElementById("STARTUP");
+      button.disabled = true;
+      
     } else {
-       alert("PASSWORD REQUIRED")
+       alert("ACCESS DENIED")
     }
    
   }
